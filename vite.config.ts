45e32ccab4fe8b0ecafab37219dd7b328e3e5755
TheJohny71 <jduval71@me.com>
2024-11-13 19:36:59 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_URL || '/',
+  base: '/alfie/', // This should match your repository name
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    sourcemap: true,
-  },
+    sourcemap: true
+  }
 })
