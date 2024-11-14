963af5be47a100c952095c60269497b7c74a6bb7
TheJohny71 <jduval71@me.com>
2024-11-13 19:23:48 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/alfie-pto-planner/',
+  base: process.env.VITE_BASE_URL || '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        entryFileNames: `assets/[name].js`,
-        chunkFileNames: `assets/[name]-[hash].js`,
-        assetFileNames: `assets/[name].[ext]`
-      }
-    }
-  }
+    sourcemap: true,
+  },
 })
